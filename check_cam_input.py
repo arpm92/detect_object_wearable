d960507e46384af7c8d20a878bdb7eb115e86017
Alejandro P <a.perez@neurologiq.de>
2022-12-19 14:26:17 +0100

    list available cameras

---
+++ check_cam_input.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def returnCameraIndexes():
+    # checks the first 10 indexes.
+    index = 0
+    arr = []
+    i = 10
+    while i > 0:
+        cap = cv2.VideoCapture(index)
+        if cap.read()[0]:
+            arr.append(index)
+            cap.release()
+        index += 1
+        i -= 1
+    return arr
+
+cam_index = returnCameraIndexes()
+
+print(cam_index)
